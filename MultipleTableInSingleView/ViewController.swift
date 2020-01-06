99251e22e5cb0867770fdbe8b9db2e5c88fb9899
Mac <sudiptoroy@sudipto.local>
2020-01-06 16:25:12 +0600

    Create Dictionary for SampleData

--- MultipleTableInSingleView/ViewController.swift
+++ MultipleTableInSingleView/ViewController.swift
@@ -12,9 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var data = [String: Array<Any>]()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        data = dataSet()
+        print(data)
     }
-
+    
+    
+    func dataSet () -> [String: Array<Any>] {
+        
+        var dataSetDict = [String: Array<Any>]()
+        
+        dataSetDict = ["A" : ["Apple", "Ant"],
+                       "B" : ["Boat", "Ball"],
+                       "C" : ["Cat", "Castle"],
+                       "D" : ["Dog", "Duck"],
+                       "E" : ["Elephant", "Empire"]
+                       
+            ] as [String : Array<Any>]
+        
+        for (key, value) in dataSetDict {
+            print("Dictionary key \(key) - Dictionary value \(value)")
+        }
+        
+        return dataSetDict
+    }
 
 }
 
