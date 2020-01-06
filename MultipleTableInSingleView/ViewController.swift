4852943f76eb5cf2533fc8ab268be4748f62984e
Mac <sudiptoroy@sudipto.local>
2020-01-06 18:56:49 +0600

    Show Keys To the first Table

--- MultipleTableInSingleView/ViewController.swift
+++ MultipleTableInSingleView/ViewController.swift
@@ -8,15 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var table1: UITableView!
+    @IBOutlet weak var table2: UITableView!
+    
+    
+    var cellIdentifier : String = "cell"
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        var data = [String: Array<String>]()
+        
+        
+        table1.delegate = self
+        table2.delegate = self
+        table1.dataSource = self
+        table2.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
         
-        data = dataSet()
-        print(data)
+        
+    }
+    
+    // function to find how many rows to generate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView.tag == 1 {
+            let data = dataSet()
+            print("In table tableView numberofrows")
+            print(data)
+            let numberOfRows = data.count
+            return numberOfRows
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath as IndexPath) as UITableViewCell
+        let data = dataSet()
+        if tableView.tag == 1 {
+            cell.textLabel?.text = Array(data.keys)[indexPath.row]
+            
+        }
+        return cell
     }
     
     // Function for Data
@@ -39,5 +74,5 @@ class ViewController: UIViewController {
         return dataSetDict
     }
 
-}
+  }
 
